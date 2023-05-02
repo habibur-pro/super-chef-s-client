6697f18f267382073a7a6867221db40eee748a9a
habibur <va.habibur@gmail.com>
2023-05-02 08:34:56 +0600

    implemented router

---
+++ src/routes/routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Blog from "../pages/Blog";
+import ChefDetails from "../pages/ChefDetails";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/chef/:id',
+                element: <ChefDetails></ChefDetails>
+            }
+        ]
+    }
+])
+export default router
