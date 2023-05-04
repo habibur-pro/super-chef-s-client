f3a3a7342725278d04ca233a35efdfd46e440cf5
habibur <va.habibur@gmail.com>
2023-05-04 12:01:24 +0600

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer</h3>
+        <div className="bg-slate-900 mt-5 text-center py-5 text-white dark:bg-neutral-600">
+            <div className=''>
+                <div className=" pt-9">
+                    <div className="mb-9 flex justify-center">
+                        <h1 className='uppercase text-4xl font-bold tracking-widest '>Super chefs</h1>
+                    </div>
+                </div>
+                <ul className='flex justify-center  gap-4 font-bold mb-5'>
+                    <li className='hover:underline duration-500 font-sembold'><Link>Home</Link></li>
+                    <li className='hover:underline duration-500 font-sembold'><Link>Feature </Link></li>
+                    <li className='hover:underline duration-500 font-sembold'><Link>Reviews</Link></li>
+                    <li className='hover:underline duration-500 font-sembold'><Link>Chefs</Link></li>
+                    <li className='hover:underline duration-500 font-sembold'><Link>Blog</Link></li>
+                    <li className='hover:underline duration-500 font-sembold'><Link>Contact Us</Link></li>
+                </ul>
+                <ul className='flex justify-center text-black my-5 mt-7  gap-4 text-lg'>
+                    <li className='p-2 bg-slate-200 rounded-full hover:-translate-y-2 duration-500'><Link> <FaFacebookF /></Link></li>
+                    <li className='p-2 bg-slate-200 rounded-full hover:-translate-y-2 duration-500'><Link> <FaInstagram /></Link></li>
+                    <li className='p-2 bg-slate-200 rounded-full hover:-translate-y-2 duration-500'><Link> <FaLinkedinIn /></Link></li>
+                    <li className='p-2 bg-slate-200 rounded-full hover:-translate-y-2 duration-500'><Link> <FaTwitter /></Link></li>
+                </ul>
+            </div>
+
         </div>
     );
 };
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,49 +5,49 @@ const Reviews = () => {
         <div className='my-container'>
             <h1 className='text-3xl text-gray-600 text-center mb-10 font-extrabold'>Flavor Feedback</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-                <blockquote class="rounded-lg bg-gray-100 p-8">
-                    <div class="flex items-center gap-4">
+                <blockquote className="rounded-lg bg-gray-100 p-8">
+                    <div className="flex items-center gap-4">
                         <img
                             alt="Man"
                             src={testFood}
-                            class="h-16 w-16 rounded-full object-cover"
+                            className="h-16 w-16 rounded-full object-cover"
                         />
                         <div>
-                            <p class="mt-1 text-lg font-medium text-gray-700">Paul Starr</p>
+                            <p className="mt-1 text-lg font-medium text-gray-700">Paul Starr</p>
                         </div>
                     </div>
-                    <p class="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
+                    <p className="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
                         voluptatem alias ut provident sapiente repellendus.
                     </p>
                 </blockquote>
-                <blockquote class="rounded-lg bg-gray-100 p-8">
-                    <div class="flex items-center gap-4">
+                <blockquote className="rounded-lg bg-gray-100 p-8">
+                    <div className="flex items-center gap-4">
                         <img
                             alt="Man"
                             src={testFood}
-                            class="h-16 w-16 rounded-full object-cover"
+                            className="h-16 w-16 rounded-full object-cover"
                         />
                         <div>
-                            <p class="mt-1 text-lg font-medium text-gray-700">Paul Starr</p>
+                            <p className="mt-1 text-lg font-medium text-gray-700">Paul Starr</p>
                         </div>
                     </div>
-                    <p class="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
+                    <p className="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
                         voluptatem alias ut provident sapiente repellendus.
                     </p>
-                </blockquote> <blockquote class="rounded-lg bg-gray-100 p-8">
-                    <div class="flex items-center gap-4">
+                </blockquote> <blockquote className="rounded-lg bg-gray-100 p-8">
+                    <div className="flex items-center gap-4">
                         <img
                             alt="Man"
                             src={testFood}
-                            class="h-16 w-16 rounded-full object-cover"
+                            className="h-16 w-16 rounded-full object-cover"
                         />
                         <div>
-                            <p class="mt-1 text-lg font-medium text-gray-700">Paul Starr</p>
+                            <p className="mt-1 text-lg font-medium text-gray-700">Paul Starr</p>
                         </div>
                     </div>
-                    <p class="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
+                    <p className="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
                         voluptatem alias ut provident sapiente repellendus.
                     </p>
