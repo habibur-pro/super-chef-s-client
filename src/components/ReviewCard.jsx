c57666e3a8c9333e959ff5668a58abc488925411
habibur <va.habibur@gmail.com>
2023-05-05 05:52:32 +0600

    added slider in revies

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const ReviewCard = ({ review }) => {
     return (
         <blockquote className="rounded-lg bg-gray-100 p-8">
-            <div className="flex items-center gap-4">
+            <div className="lg:flex items-center gap-4">
                 <img
                     alt="Man"
                     src={review.image_url}
-                    className="h-16 w-16 rounded-full object-cover"
+                    className="h-16 w-16 rounded-full object-cover mx-auto lg:mx-0 text-center lg:text-start"
                 />
                 <div>
-                    <p className="mt-1 text-lg font-medium text-gray-700">{review.name}</p>
+                    <p className="mt-1 text-lg font-medium text-center lg:text-start text-gray-700">{review.name}</p>
                 </div>
             </div>
             <p className="line-clamp-2 sm:line-clamp-none mt-4 text-gray-500">
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReviewCard from './ReviewCard';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
+// import "./styles.css";
 
+// import required modules
+import { Pagination, Autoplay } from "swiper";
 
 
 const Reviews = ({ reviews }) => {
@@ -13,14 +19,46 @@ const Reviews = ({ reviews }) => {
             <p className='text-center mt-2 mb-12 md:w-1/2 mx-auto'>
                 In our journey we have met many customers and tried to serve them as best we can. Today we are so far in their love
             </p>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'> */}
+            <Swiper
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1
+                    },
+                    640: {
+                        slidesPerView: 2
+                    },
+                    768: {
+                        slidesPerView: 3
+                    }
+                }}
+
+
+                loop={true}
+                spaceBetween={30}
+                centeredSlides={true}
+                grabCursor={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+
+
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination, Autoplay]}
+                className="mySwiper"
+            >
+
                 {
-                    reviews.map(review => <ReviewCard
+                    reviews.map(review => <SwiperSlide> <ReviewCard
                         key={review.id}
                         review={review}
-                    ></ReviewCard>)
+                    ></ReviewCard></SwiperSlide>)
                 }
-            </div>
+            </Swiper>
+            {/* </div> */}
         </div>
     );
 };
